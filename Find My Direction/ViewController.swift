9cbb7093057c4fff286e206a659765beaae6b7f7
Tadeh <tadeh19@gmail.com>
2018-04-27 19:18:03 +0430

    Initial Commit

---
+++ Find My Direction/ViewController.swift
@@ -0,0 +1,167 @@
+//
+//  ViewController.swift
+//  Find My Direction
+//
+//  Created by Tadeh Alexani on 2/7/1397 AP.
+//  Copyright © 1397 Algorithm. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import SwiftyJSON
+import Alamofire
+
+struct Place {
+    let name: String
+    let long: Double
+    let lat: Double
+    let color: UIColor
+    
+    init(name:String,long:Double,lat:Double, color: UIColor) {
+        self.name = name
+        self.long = long
+        self.lat = lat
+        self.color = color
+    }
+}
+
+class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
+    
+    let lightBlue = UIColor(hexString: "#72bcd4")
+    var isStart = false
+    var isEnd = false
+    
+    var startLocation: CLLocationCoordinate2D?
+    var destinationLocation: CLLocationCoordinate2D?
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Create places
+        let places = [
+            Place(name: "مترو سبلان", long: 51.46456 , lat: 35.71864, color: UIColor.blue),
+            Place(name: "مترو شهید مدنی", long: 51.45336 , lat: 35.70929, color: UIColor.blue),
+            Place(name: "مترو امام حسین", long: 51.44585 , lat: 35.70257, color: UIColor.blue),
+            Place(name: "مترو صیاد شیرازی", long: 51.45816 , lat: 35.73589, color: lightBlue),
+            Place(name: "مترو قدوسی", long: 51.44513 , lat: 35.73132, color: lightBlue),
+            Place(name: "مترو سهروردی", long: 51.43597 , lat: 35.73099, color: lightBlue),
+            Place(name: "مترو بهشتی", long: 51.42716 , lat: 35.73001, color: lightBlue),
+            Place(name: "مترو بهشتی", long: 51.42716 , lat: 35.73001, color: UIColor.red),
+            Place(name: "مترو مفتح", long: 51.42780 , lat: 35.72442, color: UIColor.red),
+            Place(name: "مترو هفت تیر", long: 51.42607 , lat: 35.71541, color: UIColor.red),
+            Place(name: "مترو طالقانی", long: 51.42594 , lat: 35.70719, color: UIColor.red)
+        ]
+        
+        // Create a GMSCameraPosition that tells the map to display the
+        let camera = GMSCameraPosition.camera(withLatitude: 35.723407, longitude: 51.443528, zoom: 13.0)
+        
+        mapView.camera = camera
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        
+        //Create markers
+        for place in places {
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2D(latitude: place.lat, longitude: place.long)
+            marker.title = place.name
+            marker.snippet = "Hey, this is \(place.name)"
+            marker.icon = GMSMarker.markerImage(with: place.color)
+            marker.map = mapView
+        }
+    }
+    
+    //MARK: - Location Manager delegates
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error to get location : \(error)")
+    }
+    
+    // MARK: - GMSMapViewDelegate
+    
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+        mapView.isMyLocationEnabled = true
+    }
+    
+    func mapView(_ mapView: GMSMapView, willMove gesture: Bool) {
+        mapView.isMyLocationEnabled = true
+        
+        if (gesture) {
+            mapView.selectedMarker = nil
+        }
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        mapView.isMyLocationEnabled = true
+        return false
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        
+        if isStart && !isEnd {
+            isEnd = true
+            
+            let marker = GMSMarker()
+            marker.position = coordinate
+            destinationLocation = coordinate
+            marker.icon = GMSMarker.markerImage(with: UIColor.brown)
+            marker.map = mapView
+        }
+        
+        
+        if !isStart {
+            isStart = true
+            
+            let marker = GMSMarker()
+            marker.position = coordinate
+            startLocation = coordinate
+            marker.icon = GMSMarker.markerImage(with: UIColor.cyan)
+            marker.map = mapView
+        }
+        
+    }
+    
+    func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
+        mapView.isMyLocationEnabled = true
+        mapView.selectedMarker = nil
+        return false
+    }
+    
+    @IBAction func findButtonTapped(_ sender: Any) {
+        guard let start = startLocation, let dest = destinationLocation else {
+            return
+        }
+        self.drawPath(startLocation: start, endLocation: dest)
+    }
+    
+    //MARK: - this is function for create direction path, from start location to desination location
+    func drawPath(startLocation: CLLocationCoordinate2D, endLocation: CLLocationCoordinate2D)
+    {
+        let origin = "\(startLocation.latitude),\(startLocation.longitude)"
+        let destination = "\(endLocation.latitude),\(endLocation.longitude)"
+        
+        let url = "https://maps.googleapis.com/maps/api/directions/json?origin=\(origin)&destination=\(destination)&mode=driving&key=AIzaSyCKq1LT6NcG6-CYwxiJ7kV6Q3Jp80cpAVQ"
+        
+        Alamofire.request(url).responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let json = try? JSON(data: response.data!) {
+                let routes = json["routes"].arrayValue
+                
+                for route in routes
+                {
+                    let routeOverviewPolyline = route["overview_polyline"].dictionary
+                    let points = routeOverviewPolyline?["points"]?.stringValue
+                    let path = GMSPath.init(fromEncodedPath: points!)
+                    let polyline = GMSPolyline.init(path: path)
+                    polyline.map = self.mapView
+                }
+            }
+            
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'Find My Direction' do
+    use_frameworks!
+    
+pod 'GoogleMaps'
+pod 'SwiftyJSON'
+pod 'Alamofire'
+end
